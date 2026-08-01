13ac402fb44807bfd60e585ba9856e7955dfc26a
Romie <249513315+idfwyy@users.noreply.github.com>
2026-07-31 23:19:25 -0500

    Keypair: Ed25519 identity with sign and verify

---
+++ trustsdk/identity.py
@@ -0,0 +1,36 @@
+from cryptography.hazmat.primitives.asymmetric import ed25519
+from cryptography.exceptions import InvalidSignature
+
+class Keypair:
+
+    def __init__(self):
+        self.private_key = ed25519.Ed25519PrivateKey.generate()
+        self.public_key = self.private_key.public_key()
+
+    def public_key_hex(self) -> str:
+        return self.public_key.public_bytes_raw().hex()
+
+    def sign(self, message : str) -> str:
+        return self.private_key.sign(message.encode()).hex()
+
+    @staticmethod
+    def verify(public_key_hex : str, message : str, signature_hex :str) -> bool:
+        raw = bytes.fromhex(public_key_hex)
+        key_obj = ed25519.Ed25519PublicKey.from_public_bytes(raw)
+        try:
+            key_obj.verify(bytes.fromhex(signature_hex), message.encode())
+            return True
+        except InvalidSignature:
+            return False
+
+
+
+if __name__ == "__main__":
+    kp = Keypair()
+    print(type(kp.private_key))
+    print(type(kp.public_key))
+    sig = kp.sign("hello")
+    print(sig)
+
+    print("real:", Keypair.verify(kp.public_key_hex(), "hello", sig))
+    print("fake:", Keypair.verify(kp.public_key_hex(), "hacked", sig))
